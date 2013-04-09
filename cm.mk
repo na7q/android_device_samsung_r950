d79a237af5a66109ae5134c6db8804e735cbe711
sbrissen <sbrissen@hotmail.com>
2013-04-09 13:20:30 -0400

    update target device now that audio hal config doesn't depend on it
    Change-Id: I19fb034c5e4ed8542f2bfad9e67140852399a137

--- cm.mk
+++ cm.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := SCH-R950
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lteusc TARGET_DEVICE=t0ltecdma BUILD_FINGERPRINT="samsung/t0lteusc/t0lteusc:4.1.1/JRO03C/R950VXALL1:user/release-keys" PRIVATE_BUILD_DESC="t0lteusc-user 4.1.1 JRO03C R950VXALL1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lteusc TARGET_DEVICE=t0lteusc BUILD_FINGERPRINT="samsung/t0lteusc/t0lteusc:4.1.1/JRO03C/R950VXALL1:user/release-keys" PRIVATE_BUILD_DESC="t0lteusc-user 4.1.1 JRO03C R950VXALL1 release-keys"
