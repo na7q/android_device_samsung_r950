153aff0a7e43bac597293fe6506ebcebf7686950
sbrissen <sbrissen@hotmail.com>
2013-06-17 07:32:13 -0400

    r950: update fingerprint
    Change-Id: Idaaa994d2bcee6424996ba5be832ac68a886e2dd

--- cm.mk
+++ cm.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := SCH-R950
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lteusc TARGET_DEVICE=t0lteusc BUILD_FINGERPRINT="samsung/t0lteusc/t0lteusc:4.1.2/JZO54K/R950VXAMB1:user/release-keys" PRIVATE_BUILD_DESC="t0lteusc-user 4.1.2 JZO54K R950VXAMB1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lteusc TARGET_DEVICE=t0lteusc BUILD_FINGERPRINT="samsung/t0lteusc/t0lteusc:4.1.2/JZO54K/R950VXAME2:user/release-keys" PRIVATE_BUILD_DESC="t0lteusc-user 4.1.2 JZO54K R950VXAME2 release-keys"
